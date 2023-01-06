6bd3c802b3c5fc22effbce8a589aaf2912e21cd2
condrock <93878113+condrock@users.noreply.github.com>
2023-01-06 14:27:29 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+function redirectCU(e) {
+    if (e.ctrlKey && e.which == 85) {
+        window.location.replace("/");
+        return false;
+    }
+}
+document.onkeydown = redirectCU;
