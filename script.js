6c7cebb04ebdf23eb3657f635658fdb60decb04c
condrock <93878113+condrock@users.noreply.github.com>
2023-01-13 14:33:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-    if ($(this).scrollTop() > 100) {
-        $('.back-to-top').fadeIn();
-    } else {
-        $('.back-to-top').fadeOut();
-    }
-    });
-    $('.back-to-top').click(function(){
-    $("html, body").animate({ scrollTop: 0 }, 600);
-    return false;
-    });
-});
-
 function redirectCU(e) {
     if (e.ctrlKey && e.which == 85) {
         window.location.replace("/");
